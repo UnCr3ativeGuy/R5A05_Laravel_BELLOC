a1228652fdc3eaa83d89f4b47545002ffc7c3ef5
AdrienBelloc <adrien82200@gmail.com>
2024-09-30 12:16:30 +0200

    Ajout du validator

--- app/Http/Controllers/EleveController.php
+++ app/Http/Controllers/EleveController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use App\Models\Eleve;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class EleveController extends Controller
 {
@@ -17,6 +18,21 @@ class EleveController extends Controller
     // Enregistrer un nouvel élève dans la base de données
     public function store(Request $request)
     {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'prénom' => 'required|string|max:255',
+            'date_naissance' => 'required|date',
+            'numéro_étudiant' => 'required|string|unique:eleves,numéro_étudiant',
+            'email' => 'required|email|unique:eleves,email|max:255',
+            'image' => 'nullable|image|mimes:jpeg,png,jpg,gif,svg|max:2048'
+        ]);
+
+        if ($validator->fails()) {
+            return redirect()->back()
+                ->withErrors($validator) // Ajoutez les messages d'erreur
+                ->withInput(); // Renvoyez les anciennes entrées
+        }
+
         // Enregistrer l'élève
         Eleve::create([
             'name' => $request->name,
