487b0e25cedc584ad6c17e41b09e0f73c7bde7a0
AdrienBelloc <adrien82200@gmail.com>
2024-09-30 12:30:09 +0200

    Optimisation des routes

--- routes/web.php
+++ routes/web.php
@@ -8,10 +8,11 @@ Route::get('/', function () {
 });
 
 //Eleve
-Route::get('/eleves', [EleveController::class, 'index'])->name('eleves.index');
-Route::delete('/eleves/{id}', [EleveController::class, 'destroy'])->name('eleves.destroy');
-Route::get('/eleves/{id}/edit', [EleveController::class, 'edit'])->name('eleves.edit');
-Route::put('/eleves/{id}', [EleveController::class, 'update'])->name('eleves.update');
-Route::get('/eleves/{id}', [EleveController::class, 'show'])->name('eleves.show');
-Route::get('/eleves/create', [EleveController::class, 'create'])->name('eleves.create');
-Route::post('/eleves', [EleveController::class, 'store'])->name('eleves.store');
+Route::resource('eleves', EleveController::class);
+//Route::get('/eleves', [EleveController::class, 'index'])->name('eleves.index');
+//Route::delete('/eleves/{id}', [EleveController::class, 'destroy'])->name('eleves.destroy');
+//Route::get('/eleves/{id}/edit', [EleveController::class, 'edit'])->name('eleves.edit');
+//Route::put('/eleves/{id}', [EleveController::class, 'update'])->name('eleves.update');
+//Route::get('/eleves/{id}', [EleveController::class, 'show'])->name('eleves.show');
+//Route::get('/eleves/create', [EleveController::class, 'create'])->name('eleves.create');
+//Route::post('/eleves', [EleveController::class, 'store'])->name('eleves.store');
